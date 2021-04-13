6768925cdf63d7a4d46b425f320352eefb3d85bd
Daniel Browne <daniel.browne@pusher.com>
2021-04-13 17:08:06 +0100

    Initial Commit

---
+++ ProductBrowser/ProductBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductBrowserApp.swift
+//  ProductBrowser
+//
+//  Created by Daniel Browne on 13/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ProductBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
