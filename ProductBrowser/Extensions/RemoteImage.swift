d31205b6172ce340f255a18dbb10641a297afa7c
Daniel Browne <daniel.browne@pusher.com>
2021-04-14 17:23:19 +0100

    Add documentation to 'RemoteImage'

--- ProductBrowser/Extensions/RemoteImage.swift
+++ ProductBrowser/Extensions/RemoteImage.swift
@@ -1,5 +1,11 @@
 import SwiftUI
 
+/// Asynchronously loads an remote image from a `URL`, presenting it in a view.
+///
+/// While the image is being fetched, a placeholder loading image is presented.
+/// If the image fails to load, then a placeholder failure image is presented instead.
+///
+/// Adapted from: https://www.hackingwithswift.com/forums/swiftui/loading-images/3292
 struct RemoteImage: View {
     private enum LoadState {
         case loading, success, failure
