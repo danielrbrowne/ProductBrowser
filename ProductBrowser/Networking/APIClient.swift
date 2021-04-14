f9ff39089c5ee580c7efcc765e332aeeb58da167
Daniel Browne <daniel.browne@pusher.com>
2021-04-14 17:30:16 +0100

    Add documentation for customised ISO-8601 date decoding logic

--- ProductBrowser/Networking/APIClient.swift
+++ ProductBrowser/Networking/APIClient.swift
@@ -4,6 +4,10 @@ import Foundation
 struct APIClient: APIotaClient {
     let session = URLSession.shared
 
+    /// The API returns ISO-8601 dates containing fractional seconds.
+    ///
+    /// The native `.iso8601` date decoding strategy does not handle
+    /// fractional seconds, so a custom strategy is used. See `DateDecodingStrategy+Extensions.swift`.
     var decoder: JSONDecoder {
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .iso8601WithFractionalSeconds
